ddac8d362866325c2e679f62ec258270f9f04e1c
ShevchukA <antoshka.vbg@mail.ru>
2023-02-04 18:39:29 +0300

    Set ball as a game object

--- game.js
+++ game.js
@@ -2,22 +2,120 @@ const balls = document.querySelectorAll(".ball");
 const grid = document.querySelector(".grid");
 const scoreField = document.querySelector(".score");
 
-let gridWidth = grid.clientWidth;
-let cell = gridWidth / 6;
+const ballSize = 50;
+const cell = 50;
+const gap = 20;
 
-for (let i = 0; i < balls.length; i++) {
-  balls[i].style.bottom = `${cell}px`;
-  balls[i].style.left = `${(i + 1) * cell}px`;
-  balls[i].style.transform = "translate(-50%, 50%)";
-}
+// set gamefield sizes;
+grid.style.height = 7 * cell + 8 * gap + "px";
+grid.style.width = 5 * cell + 6 * gap + "px";
 
 let gridX = grid.getBoundingClientRect().left;
-let gridY = grid.getBoundingClientRect().bottom;
+let gridY = grid.getBoundingClientRect().top;
+let gridW = grid.clientWidth;
+let gridH = grid.clientHeight;
+
+let pointsLimit = 3;
+
+function Ball() {
+  //absolute screen coordinates
+  this.x = 0;
+  this.y = 0;
+  this.points = getRundomPoints(pointsLimit);
+  this.html = createHtmlElement(this.points);
+  // get and set new points
+  this.updatePoints = function (newPoints) {
+    this.points = newPoints;
+    this.html.innerText = newPoints;
+  };
+  // set coordinates relative to gamefield
+  this.setXY = function (x, y) {
+    this.html.style.left = x + "px";
+    this.html.style.top = y + "px";
+    //update absolute screen coordinates
+    this.x = this.html.getBoundingClientRect().left;
+    this.y = this.html.getBoundingClientRect().top;
+    console.log(this.x, this.y);
+  };
+
+  function createHtmlElement(points) {
+    const ball = document.createElement("div");
+    ball.innerText = points;
+    ball.classList.add("ball");
+    ball.style.width = ballSize + "px";
+    ball.style.height = ballSize + "px";
+    return ball;
+  }
+
+  function getRundomPoints(lim) {
+    const points = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048];
+    let rundomNum = Math.trunc(Math.random() * lim);
+    return points[rundomNum];
+  }
+}
+
+function addNewBalls() {
+  let posX = 0;
+  let posY = gridH - ballSize - gap;
+  for (let i = 0; i < 5; i++) {
+    const ball = new Ball();
+    grid.appendChild(ball.html);
+    posX = gap + i * gap + i * cell;
+    ball.setXY(posX, posY);
+    ball.html.addEventListener("pointerdown", (e) => {
+      grabBall(ball, e);
+    });
+  }
+}
+
+addNewBalls();
+
+function grabBall(ball, e) {
+  let shiftX = ball.x - e.clientX;
+  let shiftY = ball.y - e.clientY;
+  console.log(shiftX, shiftY);
+
+  grid.addEventListener("pointermove", moveBall);
+
+  function moveBall(e) {
+    let mouseX = e.clientX;
+    let mouseY = e.clientY;
+    ball.setXY(mouseX - gridX + shiftX, mouseY - gridY + shiftY);
+  }
+
+  ball.html.addEventListener("pointerup", function () {
+    grid.removeEventListener("pointermove", moveBall);
+    //this.removeEventListener('pointerup')
+    this.onpointerup = null;
+  });
+
+  /*
+  shiftX = this.getBoundingClientRect().left + this.clientWidth / 2 - e.clientX;
+  shiftY =
+    this.getBoundingClientRect().bottom - this.clientHeight / 2 - e.clientY;
+  console.log(shiftX, shiftY);
+
+  grid.addEventListener("pointermove", moveBall);
+
+  function moveBall(e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+
+    //console.log(this, mouseX, gridX, shiftX);
+    balls[0].style.left = `${mouseX - gridX + shiftX}px`;
+    balls[0].style.bottom = `${gridY - mouseY - shiftY}px`;
+    balls[0].style.zIndex = "1";
+    */
+}
+
+/*
+
 let shiftX = 0;
 let shiftY = 0;
 let mouseX = 0;
 let mouseY = 0;
 
+/*
 // ?????????????
 balls[0].ondragstart = function () {
   return false;
@@ -50,9 +148,5 @@ balls[0].addEventListener("pointerdown", function (e) {
   });
 });
 
-function Ball(ball, posX, posY, points) {
-  this.ball = ball;
-  this.points = points;
-  this.rowPos = posX;
-  this.colPox = posY;
-}
+
+*/
